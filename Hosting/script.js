8d843849f72767e0c10ed9f066bf7f86e551db5f
bablikumari2 <anandbabli96@gmail.com>
2022-08-04 23:02:38 +0530

    hosting

--- Hosting/script.js
+++ Hosting/script.js
@@ -1,9 +1,34 @@
-https://www.econdb.com/api/series/RPUCUS/?format=json
 
-var url = 'https://www.econdb.com/api/series/RPUCUS/?format=json'
-async function getData(){
-    const res = await fetch(url);
-const data = await res.json()
-console.log(data.data)
-}
-getData()
+
+// var url = 'https://www.econdb.com/api/series/RPUCUS/?format=json'
+// async function getData(){
+//     const res = await fetch(url);
+// const data = await res.json()
+// console.log(data.data)
+// }
+// getData()
+
+
+// var a = 10;
+// function test(){
+//     console.log(a)
+//     var a =20;
+//     console.log(a)
+// }
+// test()
+
+// output = undefined 
+//    20
+
+
+// the variables declared inside a function are hoisted to the top. Hence variable a declared inside the test function is 
+// avaiable in the first console statement . But its value is not defined yet,hence its is undefined.
+
+// var test = function(){
+//     console.log(10)
+// }
+
+// function test(){
+//     console.log(20)
+// }
+// test()   output=10
